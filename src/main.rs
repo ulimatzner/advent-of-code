878a58eb6bfb69c6be8f65d21724538f275d0cfb
Uli Matzner <uli.matzner@gmail.com>
2021-12-08 20:59:18 +0100

    finalize fn run

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::Path;
 fn main() {
-    let file_name= "../data.input.txt";
-    println!("Number of increments: {}", run(file_name));
+    let path = Path::new("data/input.txt");
+    println!("Number of increments: {}", run(path));
 }
 
-fn run(file_name: &str) -> i32  {
-    println!("File name is {}", file_name);
-    unimplemented!();
+fn run(path: &Path) -> i32 {
+    let file = File::open(path).unwrap();
+    
+    let reader = BufReader::new(file);
+
+    let mut increments = 0;
+    let mut old = 999;
+
+    for line_res in reader.lines() {
+        let line = line_res.unwrap();
+        let new: i32 = line.parse().unwrap();
+        if new > old {
+            increments += 1;
+        }
+        old = new;
+    }
+
+    return increments;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::run;
+    use super::*;
 
     #[test]
     fn test_example() {
-        let test_file_name = "../tests/input.txt";
-        
-        let increments = run(test_file_name);
+        let test_path = Path::new("tests/input.txt");
+
+        let increments = run(test_path);
         assert_eq!(7, increments)
     }
 }
-
