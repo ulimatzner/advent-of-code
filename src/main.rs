2303174d110d470c23074c114c96b69da75ea2dc
Uli Matzner <uli.matzner@gmail.com>
2021-12-08 21:14:14 +0100

    implement part 2

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,19 @@ fn run(path: &Path) -> i32 {
     let reader = BufReader::new(file);
 
     let mut increments = 0;
+    let mut oldest = 999;
     let mut old = 999;
+    let mut current = 999;
 
     for line_res in reader.lines() {
         let line = line_res.unwrap();
         let new: i32 = line.parse().unwrap();
-        if new > old {
+        if old + current + new > oldest + old + current {
             increments += 1;
         }
-        old = new;
+        oldest = old;
+        old = current;
+        current = new;
     }
 
     return increments;
@@ -35,6 +39,6 @@ mod tests {
         let test_path = Path::new("tests/input.txt");
 
         let increments = run(test_path);
-        assert_eq!(7, increments)
+        assert_eq!(5, increments)
     }
 }
